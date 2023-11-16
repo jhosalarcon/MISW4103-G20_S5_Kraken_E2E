c020d0b002472acb9a439748e2bbeca2e37d3a29
Javier Fajardo <jd.fajardor1@uniandes.edu.co>
2023-11-16 18:50:04 -0500

    adicion de scripts con captura pantalla para v5.68

--- alternativa/features/v5.68/feature/step_definitions/e12_page_creation.js
+++ alternativa/features/v5.68/feature/step_definitions/e12_page_creation.js
@@ -1,10 +1,14 @@
-
 const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
+const fs = require('fs');
 
 let driver;
+
+const baseUrl = 'http://localhost:2368/ghost';
+const username = 'lordmkichavi@gmail.com';
+const password = 'Corinna2019*';
 
 async function initializeDriver() {
     let options = new chrome.Options();
@@ -12,16 +16,22 @@ async function initializeDriver() {
     driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 }
 
+async function takeScreenshot(stepName) {
+    let screenshot = await driver.takeScreenshot();
+    fs.writeFileSync(`screenshot/v5.68/caso12/screenshot-${stepName}.png`, screenshot, 'base64');
+}
+
 Given('the user has logged in to the Ghost Admin', async function () {
     await initializeDriver();
-    await driver.get('http://localhost:2368/ghost/#/signin');
+    await driver.get(`${baseUrl}/#/signin`);
     await driver.wait(until.elementLocated(By.css('input[name="identification"]')), 10000);
-    await driver.findElement(By.css('input[name="identification"]')).sendKeys('pruebas@correo.com');
+    await driver.findElement(By.css('input[name="identification"]')).sendKeys(username);
     await driver.wait(until.elementLocated(By.css('input[name="password"]')), 10000);
-    await driver.findElement(By.css('input[name="password"]')).sendKeys('Test654321');
+    await driver.findElement(By.css('input[name="password"]')).sendKeys(password);
     await driver.wait(until.elementLocated(By.css('button[type="submit"]')), 10000);
     await driver.findElement(By.css('button[type="submit"]')).click();
     await driver.sleep(5000);
+    await takeScreenshot('login');
 });
 
 Given('the user waits for {int} seconds', async function (seconds) {
@@ -29,20 +39,23 @@ Given('the user waits for {int} seconds', async function (seconds) {
 });
 
 When('the user navigates to the pages screen', async function () {
-    await driver.get('http://localhost:2368/ghost/#/pages');
+    await driver.get(`${baseUrl}/#/pages`);
     await driver.sleep(2000);
+    await takeScreenshot('navigate-to-pages');
 });
 
 Then('the user clicks on "New page" button', async function () {
     await driver.wait(until.elementLocated(By.xpath("//span[contains(text(), 'New page')]")), 10000);
     await driver.findElement(By.xpath("//span[contains(text(), 'New page')]")).click();
     await driver.sleep(2000);
+    await takeScreenshot('click-new-page');
 });
 
 Then('the user is on the page editor screen', async function () {
     let currentUrl = await driver.getCurrentUrl();
     assert.ok(currentUrl.includes('/editor/page'), 'The user is not on the page editor screen');
     await driver.sleep(2000);
+    await takeScreenshot('page-editor-screen');
 });
 
 Then('the user enters a title "Título de Nueva Página"', async function () {
@@ -50,13 +63,14 @@ Then('the user enters a title "Título de Nueva Página"', async function () {
     await titleTextarea.sendKeys('Título de Nueva Página');
     await driver.sleep(2000);
     await titleTextarea.sendKeys(Key.ENTER);
+    await takeScreenshot('enter-page-title');
 });
 
 Then('the user clicks the "Publish" button', async function () {
     let publishButton = await driver.wait(until.elementLocated(By.css('button.gh-btn.gh-btn-editor.gh-publish-trigger[data-test-button="publish-flow"]')), 10000);
     await publishButton.click();
+    await takeScreenshot('click-publish');
 });
-
 
 Then('the "Ready, set, publish. Share it with the world." message should be visible', async function () {
     let messageLocator = By.css('div.gh-publish-title');
@@ -64,6 +78,7 @@ Then('the "Ready, set, publish. Share it with the world." message should be visi
     await driver.wait(until.elementIsVisible(messageElement), 10000);
     let isVisible = await messageElement.isDisplayed();
     assert.strictEqual(isVisible, true, 'The message should be visible on the page');
+    await takeScreenshot('publish-message-visible');
 });
 
 AfterAll(async function () {
--- alternativa/features/v5.68/feature/step_definitions/e15_theme_verification.js
+++ alternativa/features/v5.68/feature/step_definitions/e15_theme_verification.js
@@ -1,10 +1,14 @@
-
 const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
+const fs = require('fs');
 
 let driver;
+
+const baseUrl = 'http://localhost:2368/ghost';
+const username = 'lordmkichavi@gmail.com';
+const password = 'Corinna2019*';
 
 function initializeDriver() {
     let options = new chrome.Options();
@@ -12,16 +16,22 @@ function initializeDriver() {
     driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
 }
 
+async function takeScreenshot(stepName) {
+    let screenshot = await driver.takeScreenshot();
+    fs.writeFileSync(`screenshot/v5.68/caso15/screenshot-${stepName}.png`, screenshot, 'base64');
+}
+
 Given('the user has logged in to the Ghost Admin', async function () {
     initializeDriver();
-    await driver.get('http://localhost:2368/ghost/#/signin');
+    await driver.get(`${baseUrl}/#/signin`);
     await driver.wait(until.elementLocated(By.css('input[name="identification"]')), 10000);
-    await driver.findElement(By.css('input[name="identification"]')).sendKeys('pruebas@correo.com');
+    await driver.findElement(By.css('input[name="identification"]')).sendKeys(username);
     await driver.wait(until.elementLocated(By.css('input[name="password"]')), 10000);
-    await driver.findElement(By.css('input[name="password"]')).sendKeys('Test654321');
+    await driver.findElement(By.css('input[name="password"]')).sendKeys(password);
     await driver.wait(until.elementLocated(By.css('button[type="submit"]')), 10000);
     await driver.findElement(By.css('button[type="submit"]')).click();
     await driver.sleep(5000);
+    await takeScreenshot('login');
 });
 
 Given('the user waits for {int} seconds', async function (seconds) {
@@ -29,8 +39,9 @@ Given('the user waits for {int} seconds', async function (seconds) {
 });
 
 When('the user navigates to the dashboard screen', async function () {
-    await driver.get('http://localhost:2368/ghost/#/dashboard');
+    await driver.get(`${baseUrl}/#/dashboard`);
     await driver.sleep(2000);
+    await takeScreenshot('navigate-to-dashboard');
 });
 
 Then('the user toggles the theme to dark', async function () {
@@ -40,12 +51,14 @@ Then('the user toggles the theme to dark', async function () {
         await themeToggle.click();
     }
     await driver.sleep(2000);
+    await takeScreenshot('toggle-to-dark-theme');
 });
 
 Then('the dark theme should be active', async function () {
     let themeToggle = await driver.findElement(By.css('.nightshift-toggle-container .nightshift-toggle.on'));
     let themeIsActive = await themeToggle.getAttribute('class').then((classes) => classes.includes('on'));
     assert(themeIsActive, 'The dark theme should be active.');
+    await takeScreenshot('dark-theme-active');
 });
 
 Then('the user toggles the theme to light', async function () {
@@ -55,12 +68,14 @@ Then('the user toggles the theme to light', async function () {
         await themeToggle.click();
     }
     await driver.sleep(2000);
+    await takeScreenshot('toggle-to-light-theme');
 });
 
 Then('the light theme should be active', async function () {
     let themeToggle = await driver.findElement(By.css('.nightshift-toggle-container .nightshift-toggle'));
     let themeIsActive = await themeToggle.getAttribute('class').then((classes) => !classes.includes('on'));
     assert(themeIsActive, 'The light theme should be active.');
+    await takeScreenshot('light-theme-active');
 });
 
 AfterAll(async function () {
